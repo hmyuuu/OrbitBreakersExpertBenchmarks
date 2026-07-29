a76876b7a10b1be317342e04329b54d83ac1102b
qingyunqian <qqian716@connect.hkust-gz.edu.cn>
2026-07-29 09:48:35 +0800

    perf(task-06): let Diffrax choose its initial step

--- src/solutions/task-06/solution_6.py
+++ src/solutions/task-06/solution_6.py
@@ -101,6 +101,7 @@ def forward(params, psi0, Hxy_mvp, Hfield_mvp, Htarget_mvp, config):
             ode_backend="diffrax",
             rtol=rtol,
             atol=atol,
+            dt0=None,
             max_steps=config["ode_max_steps"],
         )[-1]
 
